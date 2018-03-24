7883fed12fcb7c6297d07c9d7d19cc1493e71604
EndurableFawn <555bobe555@gmail.com>
2018-03-24 23:07:21 +0300

    Add all needed methods in Du;

--- src/main/ConsoleDu.java
+++ src/main/ConsoleDu.java
@@ -1,18 +1,62 @@
 package main;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConsoleDu {
-    public static void main(String[] args) {
-        for (String elem : args) {
-            File file = new File(elem);
-            if (file.exists()) {
-                System.out.println(file.getName() + ": " + getLength(file));
-            } else {
-                System.out.println("File " + file.getName() + " doesn't exist.");
-            }
+    private static boolean c = false;
+    private static boolean h = false;
+    private static double base = 1024.0;
+    private static String dim = "";
 
+    public static void main(String[] args) {
+        boolean check = true;
+        for (String elem : args) {
+            switch (elem) {
+                case "--si":
+                    base = 1000.0;
+                    break;
+                case "-c":
+                    c = true;
+                    break;
+                case "-h":
+                    h = true;
+                    break;
+            }
         }
+        if (c) {
+            long res = 0;
+            for (String elem : args) {
+                if (elem.equals("--si") || elem.equals("-c") || elem.equals("-h")) continue;
+                File file = new File(elem);
+                if (file.exists()) res += getLength(file);
+                else {
+                    check = false;
+                    System.out.println("File " + file.getName() + " doesn't exist.");
+                }
+
+            }
+            if (h) System.out.println("Total space: " +
+                    String.format("%.1f", human(res)) + " " + dim);
+            else System.out.println("Total space: " + String.format("%.1f", res / base));
+        } else {
+            for (String elem : args) {
+                if (elem.equals("--si") || elem.equals("-c") || elem.equals("-h")) continue;
+                File file = new File(elem);
+                if (file.exists()) {
+                    if (h) System.out.println(file.getName() + ": " +
+                            String.format("%.1f", human(getLength(file))) + " " + dim);
+                    else System.out.println(file.getName() + ": " +
+                            String.format("%.1f", getLength(file) / base));
+                } else {
+                    System.out.println("File " + file.getAbsolutePath() + " doesn't exist.");
+                    check = false;
+                }
+
+            }
+        }
+        System.exit(check ? 0 : 1);
     }
 
     private static long getLength(File file) {
@@ -25,5 +69,16 @@ public class ConsoleDu {
             length += file.length();
         }
         return length;
+    }
+
+    private static double human(long length) {
+        double len = (double) length;
+        List<String> dimensions = Arrays.asList("B", "KB", "MB", "GB", "TB");
+        dim = "B";
+        for (int i = 1; len >= base; i++) {
+            len /= base;
+            dim = dimensions.get(i);
+        }
+        return len;
     }
 }
